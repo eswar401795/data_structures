c905e0997d11e848049f89a63c41e6f3e8607969
eswar <eswar4017@hotmail.com>
2020-02-27 21:48:36 +0530

    add files

--- test.c
+++ test.c
@@ -1,15 +1,13 @@
+// ################ declaration of elements
 #include<stdio.h>
-void main(){
-    // Array declaration by specifying size and type
-    int arr1[10];
+int  main(){
+    int arr[5];
+    arr[0] = 5;
+    arr[2] = -10;
+    arr[3 / 2] = 2; // this is same as arr[1] = 2
+    arr[3] = arr[0];
     
-    // declare an array of user specified size
-    int n = 10;
-    int arr2[n];
-
-    // Array declaration by initializing elements.
-    int arr[] = {10, 20, 30, 40 ,50};
+    printf("%d %d %d %d", arr[0], arr[1], arr[2], arr[3]);
     
-    // Array declaration by specifying size and initializing
-    int arr[6] = {10, 20, 30, 40}; // {10, 20, 30, 40 0, 0}
+    return 0;
 }
