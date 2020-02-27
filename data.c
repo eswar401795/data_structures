713c4ca6c51de02dd6ec7486a74a21832b3da830
eswar <eswar4017@hotmail.com>
2020-02-27 21:39:10 +0530

    arrays

---
+++ data.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main(){
+    // Array declaration by specifying size and type
+    int arr1[10];
+    
+    // declare an array of user specified size
+    int n = 10;
+    int arr2[n];
+
+    // Array declaration by initializing elements.
+    int arr[] = {10, 20, 30, 40 ,50};
+    
+    // Array declaration by specifying size and initializing
+    int arr[6] = {10, 20, 30, 40}; // {10, 20, 30, 40 0, 0}
+}
